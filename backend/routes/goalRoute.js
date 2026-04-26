4a1ce825e94759a24c6581bbd962dfb0e7c21bfa
Oluwamayowa <ademayo234@gmail.com>
2026-04-25 23:33:37 -0700

    added a route folder and made each route to it's own file and also changed the port

---
+++ backend/routes/goalRoute.js
@@ -0,0 +1,10 @@
+const express = require("express")
+router = express.Router()
+
+
+router.get('/api/goals', (req, res) => {
+    res.status(200).json({ message: 'Get goals' })
+})
+
+
+module.exports = router
