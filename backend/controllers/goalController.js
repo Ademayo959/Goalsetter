c290e81ab746bc9373c99003005d5b9f8c8f47bb
Oluwamayowa <ademayo234@gmail.com>
2026-04-27 15:51:51 -0700

    connected it to mongoDb

--- backend/controllers/goalController.js
+++ backend/controllers/goalController.js
@@ -1,34 +1,36 @@
+const asyncHandler = require('express-async-handler')
+
 // @desc Get Goals
 // @route GET /api/goals
 // @access Private
 
-const getGoals = async (req, res) => {
+const getGoals = asyncHandler(async (req, res) => {
     res.status(200).json({ message: 'Get goals' })
-}
+})
 
 // @desc Create Goals
 // @route POST /api/goals
 // @access Private
 
-const setGoals = async (req, res) => {
+const setGoals = asyncHandler(async (req, res) => {
     res.status(200).json({ message: 'Set goals' })
-}
+})
 
 // @desc Update Goals
 // @route PUT /api/goals/:id
 // @access Private
 
-const updateGoals = async (req, res) => {
+const updateGoals = asyncHandler(async (req, res) => {
     res.status(200).json({ message: `Update goal ${req.params.id}` })
-}
+})
 
 // @desc Delete Goals
 // @route DELETE /api/goals/:id
 // @access Private
 
-const deleteGoals = async (req, res) => {
-    res.status(200).json({ message: `Delete goal ${req.params.id}` })
-}
+const deleteGoals = asyncHandler(async (req, res) => {
+    res.status(200).json({ message: `Delete goal ${req.params.id}` }) 
+})
 
 module.exports = {
     getGoals,
--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,11 @@
 const express = require("express")
+const colors = require('colors')
+const connectDb = require('./config/db')
 const dotenv = require("dotenv").config()
-const {errorHandler} = require("./middleware/errorMiddleware ")
+const {errorHandler} = require("./middleware/errorMiddleware")
 const port = process.env.PORT || 5000
 
-
+connectDb()
 const app = express()
 
 app.use(express.json())
@@ -11,6 +13,6 @@ app.use(express.urlencoded({extended: false}))
 
 
 app.use('/api/goals', require('./routes/goalRoute'))
-app.use(errorHandler) 
+app.use(errorHandler)
 
 app.listen(port, () => console.log(`server is running on port ${port}`))
